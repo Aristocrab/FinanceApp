0cb202f3cd37e29483cec485a5d75e8c31f58069
Aristocrab <maksymkanupa@gmail.com>
2022-11-25 10:19:25 +0200

    Moved backend

---
+++ FinanceApp.Backend/FinanceApp.Application/FinanceAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FinanceApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Application;
+
+public class FinanceAppDbContext : DbContext
+{
+    public DbSet<Account> Accounts { get; set; } = null!;
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Transaction> Transactions { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+}

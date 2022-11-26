ea5d1f3d4e3a2015d0a514bb4325b0af608453f8
Aristocrab <maksymkanupa@gmail.com>
2022-11-26 20:23:19 +0200

    Update FinanceAppDbContext.cs

--- FinanceApp.Backend/FinanceApp.Application/FinanceAppDbContext.cs
+++ FinanceApp.Backend/FinanceApp.Application/FinanceAppDbContext.cs
@@ -3,10 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FinanceApp.Application;
 
-public class FinanceAppDbContext : DbContext
+public sealed class FinanceAppDbContext : DbContext
 {
     public DbSet<Account> Accounts { get; set; } = null!;
     public DbSet<Category> Categories { get; set; } = null!;
     public DbSet<Transaction> Transactions { get; set; } = null!;
     public DbSet<User> Users { get; set; } = null!;
+
+    public FinanceAppDbContext(DbContextOptions<FinanceAppDbContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
 }

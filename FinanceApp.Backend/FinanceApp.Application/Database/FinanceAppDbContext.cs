cc7f7c12bb138737afe549ebb3612bf2220b44e5
Aristocrab <maksymkanupa@gmail.com>
2022-12-25 17:28:23 +0200

    Update FinanceAppDbContext.cs

--- FinanceApp.Backend/FinanceApp.Application/Database/FinanceAppDbContext.cs
+++ FinanceApp.Backend/FinanceApp.Application/Database/FinanceAppDbContext.cs
@@ -7,8 +7,7 @@ public sealed class FinanceAppDbContext : DbContext
 {
     public DbSet<Account> Accounts { get; set; } = null!;
     public DbSet<Category> Categories { get; set; } = null!;
-    public DbSet<Transaction> 
-        Transactions { get; set; } = null!;
+    public DbSet<Transaction> Transactions { get; set; } = null!;
     public DbSet<User> Users { get; set; } = null!;
 
     public FinanceAppDbContext(DbContextOptions<FinanceAppDbContext> options)
